4424a84cd2afbcc2ba162a8345f708603f50fe4c
ZionMatthews <54289230+zionmatthews@users.noreply.github.com>
2024-04-03 11:20:05 -0500

    Initial commit

---
+++ Source/Tophet/TophetGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TophetGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATophetGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATophetGameMode();
+};
+
+
+
